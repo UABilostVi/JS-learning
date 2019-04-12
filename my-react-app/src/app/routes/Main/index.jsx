be6f2956b1ec6109156737918ecf8091e987b1eb
UABilostVi <bilostotskyi1991@gmail.com>
2019-04-12 23:41:52 +0300

    add some changes

--- my-react-app/src/app/routes/Main/index.jsx
+++ my-react-app/src/app/routes/Main/index.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import { UserItem } from "./UserItem";
 import { Container } from "reactstrap";
-
-const users = [
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	},
-	{
-		login: "defunkt",
-		avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4"
-	}
-];
+import axios from 'axios';
 
 export class Main extends React.Component {
+	state = {
+		users: []
+	};
+	componentDidMount(){
+		axios.get('https://api.github.com/users')
+		.then(response => {
+			 const { data } = response;
+			 this.setState({ users:data });
+		})
+		.catch((err)=>console.log('something wrong'));
+	}
+
+
 	render() {
+		const { users } = this.state;
 		return (
-			<main className="main">
-				<Container>
+			<main className="main" onClick={this.onClickHandler}>
+				<Container id="test">
 					{users.map((user, id) => (
 						<UserItem user={user} key={id} />
 					))}
